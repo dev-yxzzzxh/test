b05a1f082a4911a6f5429cc533d5943039230bd1
Jeonghyeon Yang <yxzzzxh@gmail.com>
2021-06-29 13:18:00 +0900

    [feat] Implement Sum function and test file of this function

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module example.com/hello
+module test
 
 go 1.15
 
--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import ( "fmt"
          "rsc.io/quote"
+		 "test/pkg/calc"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
+	fmt.Println(calc.Sum(1,2))
 }
